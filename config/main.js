4940d5bb1c584945222aede1313d342bb499a890
Aris Jirat Kurniawan <arisjiratkurniawan@gmail.com>
2018-03-18 10:34:08 +0700

    change port

--- config/main.js
+++ config/main.js
@@ -1,4 +1,4 @@
-export const port = 3080
+export const port = 62542
 export const localhost = 'http://localhost'
 export const devServer = (port) ? localhost + ':' + port : localhost
 export const timeout = 0
